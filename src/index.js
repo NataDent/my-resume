8963cb621013b6acd63e1b9fe8e4c2586d6b8503
NataDent <cherkashyna.n@gmail.com>
2024-05-24 12:56:43 +0300

    Preparation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './index.html';
 import './index.scss';
-import code from './img/code.png';
+
 import { mult, sum } from './modules/calc.js';
 
-const imgWrap = document.querySelector( '.img' );
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append( img );
+// const imgWrap = document.querySelector( '.img' );
+// const img = new Image();
+// img.src = code;
+// img.width = 700;
+// imgWrap.append( img );
 
 console.log( mult( 3, 4 ) );
 console.log( sum( 3, 4 ) );
